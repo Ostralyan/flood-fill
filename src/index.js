1d6237e64838da35740f7f1f4e92dfbe37d5332e
Luke Xu <lukecxu@gmail.com>
2018-10-28 14:08:28 -0400

    Got it working in a surprisingly little amount of work

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
 class Square extends React.Component {
     constructor(props) {
         super(props);
@@ -15,8 +13,10 @@ class Square extends React.Component {
         backgroundColor: this.props.color
       }
       return (
-        <button className="square" style={divStyle}>    
-          {/* TODO */}
+        <button 
+            className="square" 
+            style={divStyle} 
+            onClick={() => this.props.onClick()}>    
         </button>
       );
     }
@@ -29,39 +29,86 @@ class Square extends React.Component {
           squares: [
               [
                 {
-                    color: this.getColor()
+                    color: this.getColor(),
+                    visited: false,
                 },
                 {
-                    color: this.getColor()
+                    color: this.getColor(),
+                    visited: false,
                 },
                 {
-                    color: this.getColor()
-                },
-              ],
-              [
-                {
-                    color: this.getColor()
-                },
-                {
-                    color: this.getColor()
-                },
-                {
-                    color: this.getColor()
+                    color: this.getColor(),
+                    visited: false,
                 },
               ],
               [
                 {
-                    color: this.getColor()
+                    color: this.getColor(),
+                    visited: false,
                 },
                 {
-                    color: this.getColor()
+                    color: this.getColor(),
+                    visited: false,
                 },
                 {
-                    color: this.getColor()
+                    color: this.getColor(),
+                    visited: false,
+                },
+              ],
+              [
+                {
+                    color: this.getColor(),
+                    visited: false,
+                },
+                {
+                    color: this.getColor(),
+                    visited: false,
+                },
+                {
+                    color: this.getColor(),
+                    visited: false,
                 },
               ]
           ]
         };
+    }
+
+    floodFill(i, j) {
+        const oldColor = this.state.squares[i][j].color;
+        const newColor = this.getColor();
+        const squares = this.state.squares.slice();
+
+        this.getNextFloodFillState(squares, i, j, oldColor, newColor);
+        this.clearVisisted(squares);
+        this.setState({squares: squares});
+    }
+
+    clearVisisted(squares) {
+        for(let i = 0; i < squares.length; i++) {
+            for(let j = 0; j < squares[i].length; j++) {
+                squares[i][j].visited = false;
+            }
+        }
+    }
+
+    getNextFloodFillState(squares, i, j, oldColor, newColor) {
+
+        // check out of bounds
+        if (i < 0 || i > 2) return;
+        if (j < 0 || j > 2) return;
+        // check if it's visited
+        if (squares[i][j].visited) return;
+        // Indicate node has been visited
+        squares[i][j].visited = true;
+        // check if it's same color
+        if (squares[i][j].color !== oldColor) return;
+        // set the current color to the new color and mark node as visited.
+        squares[i][j].color = newColor;
+
+        this.getNextFloodFillState(squares, i + 1, j, oldColor, newColor);
+        this.getNextFloodFillState(squares, i - 1, j, oldColor, newColor);
+        this.getNextFloodFillState(squares, i, j + 1, oldColor, newColor);
+        this.getNextFloodFillState(squares, i, j - 1, oldColor, newColor);
     }
 
     getColor() {
@@ -84,7 +131,10 @@ class Square extends React.Component {
     }
 
     renderSquare(i, j) {
-      return <Square color={this.state.squares[i][j].color}/>;
+      return <Square 
+            color={this.state.squares[i][j].color}
+            onClick={() => this.floodFill(i, j)}
+        />;
     }
   
     render() {
@@ -115,6 +165,9 @@ class Square extends React.Component {
       return (
         <div className="game">
           <div className="game-board">
+            <p>
+                Instructions: Click on any square.
+            </p>
             <Board />
           </div>
           <div className="game-info">
